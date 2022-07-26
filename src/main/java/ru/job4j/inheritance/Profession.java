9dd86688cb6d75a62a65001ce2b3f4ec4eea57bb
Eugene Vovk <eugenevovk@gmail.com>
2022-07-26 20:48:32 +0300

    add inheritance

---
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class Profession {
+    private boolean degree;
+
+    public Profession(boolean degree) {
+        this.degree = degree;
+    }
+
+    public Profession() {
+
+    }
+}
