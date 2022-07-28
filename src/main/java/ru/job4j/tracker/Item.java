c80d842b23a959022f885305b7e7f3daa0a7c8bf
Eugene Vovk <eugenevovk@gmail.com>
2022-07-28 22:09:07 +0300

    upd Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * /**
@@ -10,6 +11,8 @@ import java.time.LocalDateTime;
  * * Поле created содержит дату создания заявления.
  */
 public class Item {
+    private static final DateTimeFormatter FORMATTER
+            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
     private final LocalDateTime created = LocalDateTime.now();
@@ -44,5 +47,14 @@ public class Item {
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
