f7a1c377f03f21f353054c0565a7d4f96c46c9d7
Eugene Vovk <eugenevovk@gmail.com>
2022-07-31 00:40:15 +0300

    upd StartUI

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +16,43 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         Item actual = tracker.findAll()[0];
         assertThat(actual.getName()).isEqualTo(expected.getName());
+    }
+
+    /**
+     * Порядок действий.
+     * 1. Создаем объект tracker.
+     * 2. Создаем объект item.
+     * 3. Добавляем item в tracker. После этой операции у нас есть id.
+     * 4. Достаем item.id и создаем массив с ответами пользователя.
+     * 5. Вызываем тестируемый метод editItem. Это действие изменит состояние объекта tracker.
+     * 6. Ищем по item.id замененный item в объекте tracker.
+     * 7. Сравниваем имя найденной заявки с ожидаемой.
+     */
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Item item = new Item("new item");
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "delete item"
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item actual = tracker.findById(item.getId());
+        Assertions.assertNull(actual);
     }
 }
