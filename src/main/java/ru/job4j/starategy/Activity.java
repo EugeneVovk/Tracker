db58b5b68dae2f945ed603789cc06ce29c02e6eb
Eugene Vovk <eugenevovk@gmail.com>
2022-07-31 01:23:16 +0300

    add strategy pattern

---
+++ src/main/java/ru/job4j/starategy/Activity.java
@@ -0,0 +1,8 @@
+package ru.job4j.starategy;
+
+/**
+ * Активности, которые есть у разработчика
+ */
+public interface Activity {
+    void justDoIt();
+}
---
+++ src/main/java/ru/job4j/starategy/Developer.java
@@ -0,0 +1,22 @@
+package ru.job4j.starategy;
+
+/**
+ * Модель разработчика
+ */
+public class Developer {
+    private Activity activity;
+
+    /**
+     * Метод изменения активности
+     */
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Метода выполнения активности
+     */
+    public void executeActivity() {
+        activity.justDoIt();
+    }
+}
